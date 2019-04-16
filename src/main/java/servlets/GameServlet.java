91435a145faa1a4322ee92d65ef50fc0509aef1b
Jakub Pfajfer <jpfajfer@gmail.com>
2019-04-16 14:11:27 +0200

    Game updated

--- src/main/java/servlets/GameServlet.java
+++ src/main/java/servlets/GameServlet.java
@@ -16,15 +16,39 @@ public class GameServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession session = request.getSession();
-        List<String> words;
         synchronized (session) {
             String word = (String) session.getAttribute("word");
             String wordMask = (String) session.getAttribute("wordMask");
-            String letter = (String) request.getAttribute("letter");
+            String letter = (String) request.getParameter("letter");
+            boolean rightAnswer = false;
+            if (letter.length() == word.length()) {
+                if (letter.equals(word)) {
+                    rightAnswer = true;
+                    session.setAttribute("wordMask", word);
+                }
+            } else if (letter.length() == 1) {
+                StringBuilder updatedWordMask = new StringBuilder(wordMask);
+                for (int i = 0; i < word.length(); i++) {
+                    if (word.charAt(i) == letter.charAt(0)) {
+                        rightAnswer = true;
+                        updatedWordMask.setCharAt(i*2, word.charAt(i));
+                    }
+                }
+                session.setAttribute("wordMask", updatedWordMask.toString());
+            }
 
+            Integer lifes = (Integer) session.getAttribute("lifes");
+            Integer points = (Integer) session.getAttribute("points");
+            if (!rightAnswer) {
+                lifes--;
+                session.setAttribute("lifes", lifes);
+            } else {
+                points++;
+                session.setAttribute("points", points);
+            }
         }
-
-
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("game.jsp");
+        requestDispatcher.forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -32,20 +56,18 @@ public class GameServlet extends HttpServlet {
         HttpSession session = request.getSession();
         List<String> words;
         synchronized (session) {
-            String word = (String) session.getAttribute("word");
-            if (word == null) {
-//                    words = (List<String>) session.getAttribute("wordList");
-                words = new ArrayList<>();
-                words.add("abecadlo");
-                words.add("zpieca");
-                words.add("spadlo");
-                words.add("def");
-                word = words.get(new Random().nextInt(words.size()));
-                String wordMask;
-                wordMask = word.replaceAll("\\w", "_");
-                session.setAttribute("word", word);
-                session.setAttribute("wordMask", wordMask);
-            }
+            String word;
+            words = (List<String>) session.getAttribute("list");
+            word = words.get(new Random().nextInt(words.size()));
+            String wordMask;
+            wordMask = word.replaceAll("\\w", "_ ");
+            session.setAttribute("word", word);
+            session.setAttribute("wordMask", wordMask);
+
+            Integer lifes = 9;
+            session.setAttribute("lifes", lifes);
+            Integer points = 0;
+            session.setAttribute("points", points);
         }
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("game.jsp");
         requestDispatcher.forward(request, response);
