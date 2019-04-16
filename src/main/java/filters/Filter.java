8652ba733aafa9e5964b9295a8ffc6cb2837f437
Mateusz Pernal <exomat@wp.pl>
2019-04-16 13:08:49 +0200

    addd filter

---
+++ src/main/java/filters/Filter.java
@@ -0,0 +1,36 @@
+package filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "Filter", urlPatterns = {"/game"})
+public class Filter implements javax.servlet.Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpSession session = request.getSession(false);
+        if (session == null)
+            response.sendRedirect("index.jsp");
+        String login = (String) session.getAttribute("login");
+        if (login == null) {
+            response.sendRedirect("login.jsp");
+        } else {
+            if (!login.equals("admin"))
+                response.sendRedirect("/game");
+            else response.sendRedirect("/admin");
+            ;
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
