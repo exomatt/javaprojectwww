c8ddb884cc828d4f258637687ec8faa3dd5d15c7
Mateusz Pernal <exomat@wp.pl>
2019-04-16 13:59:06 +0200

    add word list

--- src/main/java/servlets/LanguageServlet.java
+++ src/main/java/servlets/LanguageServlet.java
@@ -1,21 +1,34 @@
 package servlets;
 
+import utils.FileManager;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "LanguageServlet", urlPatterns = "/chooseLanguage")
 public class LanguageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String language = request.getParameter("language");
+        HttpSession session = request.getSession();
         if (language.equals("PL")) {
-            //todo pobranie słow polskich ustawienie ciasteczka
+            List<String> allWords = FileManager.getAllWords(FileManager.PL);
+            if (!allWords.isEmpty())
+                synchronized (session) {
+                    session.setAttribute("list", allWords);
+                }
         } else if (language.equals("ENG")) {
-            //todo pobranie słow angielskich
+            List<String> allWords = FileManager.getAllWords(FileManager.ENG);
+            if (!allWords.isEmpty())
+                synchronized (session) {
+                    session.setAttribute("list", allWords);
+                }
         }
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("login.jsp");
         requestDispatcher.forward(request, response);
